8e209a96d8466d4c6f3945a0c75a794d77c80a8f
ccboys <walter@intrising.com.tw>
2018-06-05 22:52:01 +0800

    import webpack

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [{ loader: "html-loader", options: { minimize: true } }]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader", // creates style nodes from JS strings
+          "css-loader", // translates CSS into CommonJS
+          "sass-loader" // compiles Sass to CSS
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "src/index.html",
+      filename: "./index.html"
+    })
+  ]
+};
