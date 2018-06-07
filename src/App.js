0b5ac79960342e49ef411167e00a9e0773cfe976
ccboys <walter@intrising.com.tw>
2018-06-08 00:49:03 +0800

    [MainSection] added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 import './scss/_main.scss'
 import Header from './Header/Header.js'
+import MainSection from './MainSection/MainSection.js'
 // name: 0-8-4
 
 class App extends Component {
   render() {
     return (
-      <Header></Header>
+      <div >
+        <Header></Header>
+        <MainSection></MainSection>
+      </div>
     );
   }
 }
---
+++ src/MainSection/MainSection.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './MainSection.scss'
+
+class MainSection extends Component {
+  render () {
+    return (
+      <div className="main_section">
+      </div>
+    )
+  }
+}
+
+export default MainSection
